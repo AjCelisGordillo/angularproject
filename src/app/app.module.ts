c46586b1a490d6af5bd893ecf88736d4b415d190
Alvaro C. Gordillo <alvaro.gordillo@massygroup.com>
2024-12-13 16:22:49 -0500

    angularEcommerce

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+import { environment } from '../environments/environment.dev';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase.config),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  name: 'dev',
+  firebase: {
+    config: {
+      apiKey: "AIzaSyAuuXCAnqmXekH0dqrh9gbg6sU_YJYB7cw",
+      authDomain: "ecommerce-6fbed.firebaseapp.com",
+      projectId: "ecommerce-6fbed",
+      storageBucket: "ecommerce-6fbed.appspot.com",
+      messagingSenderId: "319504817905",
+      appId: "1:319504817905:web:7b85e96bf397bb99a75350"
+    }
+  }
+};
